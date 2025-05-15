ea4efb2a15883688557215ebb55f4752afd32898
Cbecerril55 <cbecerrilv55@gmail.com>
2025-05-15 14:00:27 -0500

    Se agregan consultas y procedimientos sobre las mascotas

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const verificarToken = require("../middlewares/authMiddleware");
-const { crearUsuario, obtenerUsuarios, loginUsuario, eliminarUsuario } = require('../controllers/usuarios');
+const { crearUsuario, obtenerUsuarios, loginUsuario, eliminarUsuario, actualizarUsuario } = require('../controllers/usuarios');
 
 router.get('/', verificarToken, obtenerUsuarios);
 
 router.post('/', crearUsuario);
 router.post('/login', loginUsuario);
+
+router.put('/:id', verificarToken, actualizarUsuario);
 
 router.delete("/:id", verificarToken, eliminarUsuario);
 
